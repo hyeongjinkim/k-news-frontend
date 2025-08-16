b919c37005470d9e8f9e4ed08f2dc97d80418b7f
hj.kim <hj.kim@intelliia.co>
2025-08-16 20:42:53 +0900

    package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
-  // ⬇️ 이 modules 부분을 추가/수정합니다.
   modules: [
     '@nuxtjs/tailwindcss'
   ],
-
   routeRules: {
     '/api/**': {
       proxy: 'http://45.76.144.201/api/**'
     }
+  },
+
+  // ⬇️ 이 부분을 추가해주세요.
+  generate: {
+    exclude: [
+      '/'
+    ]
   }
 })
