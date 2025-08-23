c50b74c3ac678ea79ca1e04eca4a3c431829fea9
hj.kim <hj.kim@intelliia.co>
2025-08-24 03:05:02 +0900

    Final fix for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,15 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   modules: [
     '@nuxtjs/tailwindcss'
   ],
-
-  // 🔽 이 부분을 추가해주세요!
   css: [
     '~/assets/css/tailwind.css',
   ],
-
   routeRules: {
     '/api/**': {
-      proxy: 'http://127.0.0.1:8000/api/**' 
+      proxy: 'http://127.0.0.1:8000/api/**'
     }
   }
 })
