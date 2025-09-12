f6baee4d28dc05372bb2c17e06549b0abafa47c2
hj.kim <hj.kim@intelliia.co>
2025-09-12 23:14:46 +0900

    d

--- pages/[lang]/index.vue
+++ pages/[lang]/index.vue
@@ -160,7 +160,6 @@ watch(searchQuery, (newValue) => {
           <svg class="w-4 h-4 absolute right-2 top-1/2 -translate-y-1/2 pointer-events-none text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
         </div>
       </div>
-      <FeaturedSection :current-lang="currentLang" />
       <div class="relative">
         <input 
           type="search" 
@@ -171,6 +170,7 @@ watch(searchQuery, (newValue) => {
         <svg class="w-4 h-4 absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
       </div>
     </header>
+    <FeaturedSection :current-lang="currentLang" />
     <main class="p-2">
       <div v-if="isLoading" class="p-8 text-center text-gray-500">Loading...</div>
       <div v-else-if="error" class="p-4 text-center text-red-500">Failed to load articles.</div>
